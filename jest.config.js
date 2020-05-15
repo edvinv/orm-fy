d55d0312c7b34d6818f37c3988863030caf4d3c7
edvinv <edvinvidmar@hotmail.com>
2020-05-15 14:23:28 +0200

    Ignore some directories in jest testing.

--- jest.config.js
+++ jest.config.js
@@ -5,5 +5,17 @@ module.exports = {
     'ts-jest': {
       tsConfig: 'tsconfig.test.json'
     }
-  }
+  },
+  testPathIgnorePatterns: [
+    "<rootDir>/test/.vscode/",
+    "<rootDir>/test/build/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/test/environments/",
+  ],
+  watchPathIgnorePatterns:[
+    "<rootDir>/test/.vscode/",
+    "<rootDir>/test/build/",
+    "<rootDir>/node_modules/",
+    "<rootDir>/test/environments/",
+  ]
 };
